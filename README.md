84cdb645b500375cd0c686b53c04e7b21a45ea82
Richard Eriksson <richard@justagwailo.com>
2014-01-24 11:45:08 -0800

    Initial commit.

---
+++ README.md
@@ -0,0 +1,14 @@
+Minecraft Holmes
+==========
+
+Requires [node](http://nodejs.org/) and [npm](http://npmjs.org/). You also need a Twitter App access token, consumer key, and associated secrets: https://dev.twitter.com/apps/new
+
+Before running the program:
+
+1. Add all the Twitter info to twitter.js
+2. Install the following Node modules:
+  * npm install xml2js
+  * npm install twit
+4. node minecraftholmes
+
+Consider running the bot in the background with [forever](https://github.com/nodejitsu/forever) or [pm2](https://github.com/Unitech/pm2).
---
+++ minecraftholmes.js
@@ -0,0 +1,33 @@
+var Twit = require('twit');
+var T = new Twit(require(__dirname + '/twitter.js'));
+
+// Get a random integer between min and max http://stackoverflow.com/a/12885196/300278
+function getRandomInt(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+var http = require('http');
+setInterval(function() {
+  var parseString = require('xml2js').parseString;
+  http.get('http://thegamesdb.net/api/GetGame.php?id=' + getRandomInt(0, 20000), function(res) {
+    res.on('data', function(d) {
+      xml = d.toString();
+      console.log(xml);
+      try { 
+        parseString(xml, function(err, result) {
+          if (typeof(result.Data) != "undefined") {
+            game = result.Data.Game[0].GameTitle[0] + " Holmes";
+            console.log(game);
+            T.post('statuses/update', { status: game}, function(err, reply) {
+              console.log("error: " + err);
+              console.log("reply: " + reply);
+            });
+          }
+        });
+      }
+      catch (err) {
+        console.log("Whale!: " + err);
+      }
+    });
+  });
+}, 3600000);
